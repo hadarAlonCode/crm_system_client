3ff01408e89ee40ba0b91a74180295d443444cbe
Hadar Alon <hadaralon3@gmail.com>
2020-04-09 21:19:17 +0300

    fixed css

--- src/components/Dashboard/Parts/Contacts/Parts/Contact.jsx
+++ src/components/Dashboard/Parts/Contacts/Parts/Contact.jsx
@@ -11,14 +11,14 @@ class Contact extends Component {
             <Fade >
 
                 <div className="contact__container">
-                    <div>
+                    <div className="conatct_text">
                         <div className="icon__container">{contact.img ? <img src={contact.img} alt="Smiley face" height="42" width="42"></img> : <i class="fas fa-user-circle"></i>} </div>
                         <div className="name__container">{contact.name ? contact.name : "-"}</div>
                     </div>
 
-                    <div>{contact.email ? contact.email : "-"}</div>
-                    <div>{contact.company ? contact.company : "-"}</div>
-                    <div>{contact.status ? contact.status : "-"}</div>
+                    <div className="conatct_text">{contact.email ? contact.email : "-"}</div>
+                    <div className="conatct_text conatct_company">{contact.company ? contact.company : "-"}</div>
+                    <div className="conatct_text">{contact.status ? contact.status : "-"}</div>
                     {/* <div>{contact.firstContact ? moment(contact.firstContact).format('L') : "-"}</div> */}
                 </div>
             </Fade>
