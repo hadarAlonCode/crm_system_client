1bf85ab32148c0129c9f0406047be91679b7f69e
Hadar Alon <hadaralon3@gmail.com>
2020-04-22 18:45:58 +0300

    adding README and Clean Code

--- src/components/Redirector/Redirector.jsx
+++ src/components/Redirector/Redirector.jsx
@@ -5,7 +5,8 @@ import * as actions from '../../actions/actions';
 import { BrowserRouter as Router, Route, Link, Switch, withRouter } from "react-router-dom";
 import { DASHBOARD_OVERVIEW , LOGIN} from '../../tools/routs';
 import { getCookie } from '../../tools/cookie/cookie';
-//redirector - make the login - check if cookie--> go to dashboard or anoter location history -- else --> go to login page.
+
+//redirector - make the login - check if cookie --> go to dashboard \ location history -- else --> go to login page.
 
 class Redirector extends Component {
     constructor() {
